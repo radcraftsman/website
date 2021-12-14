a931e95a723e54e5e831450a095bf1688663e914
Jacob Wehrle <chas@radcraftsman.ca>
2021-12-13 21:17:30 -0500

    fix: gallery to chas spec

--- js/scripts.js
+++ js/scripts.js
@@ -46,20 +46,19 @@
     // Collapse the navbar when page is scrolled
     $(window).scroll(navbarCollapse);
 
-    // Magnific popup calls
-    $('#portfolio').magnificPopup({
-        delegate: 'a',
-        type: 'image',
-        tLoading: 'Loading image #%curr%...',
-        mainClass: 'mfp-img-mobile',
-        gallery: {
-            enabled: true,
-            navigateByImgClick: true,
-            preload: [0, 1]
-        },
-        image: {
-            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-        }
-    });
+    // new portfolio style
+    $('a.portfolio-box').on('click', function(event) {
+		event.preventDefault();
+		
+		var gallery = $(this).attr('href');
+    
+		$(gallery).magnificPopup({
+      delegate: 'a',
+			type:'image',
+			gallery: {
+				enabled: true
+			}
+		}).magnificPopup('open');
+	});
 
 })(jQuery); // End of use strict
