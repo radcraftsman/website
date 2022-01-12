09aeb0a9bcbf14396e9c181176abe0151000900c
Jacob Wehrle <chas@radcraftsman.ca>
2022-01-11 21:20:16 -0500

    test: changing the email up a bit

--- form/form.js
+++ form/form.js
@@ -4,41 +4,28 @@ $(document).ready(function () {
         $(".help-block").remove();
 
         var formData = {
-        name: $("#name").val(),
-        email: $("#email").val(),
-        subject: $("#subject").val(),
-        message: $("#message").val(),
+            name: $("#name").val(),
+            email: $("#email").val(),
+            subject: $("#subject").val(),
+            message: $("#message").val(),
         };
   
-        $.post('contact.php', function(formData) {
+        $.ajax({
+            type: "POST",
+            url: "https://radcraftsman.ca/form/contact.php",
+            data: formData,
+            dataType: "json",
+            encode: true,
+            success: function(data) { console.log("success") },
+            error: function(ts) { console.log("failed") }
+        }).done(function (data) {
             console.log(data);
 
             if (!data.success) {
-                if (data.errors.name) {
-                    $("#name-group").addClass("has-error");
-                    $("#name-group").append(
-                    '<div class="help-block">' + data.errors.name + "</div>"
-                    );
-                }
-                if (data.errors.email) {
-                    $("#email-group").addClass("has-error");
-                    $("#email-group").append(
-                    '<div class="help-block">' + data.errors.email + "</div>"
-                    );
-                }
-                if (data.errors.subject) {
-                    $("#subject-group").addClass("has-error");
-                    $("#subject-group").append(
-                    '<div class="help-block">' + data.errors.subject + "</div>"
-                    );
-                }
-                if (data.errors.message) {
-                    $("#message-group").addClass("has-error");
-                    $("#message-group").append(
-                        '<div class="help-block">' + data.errors.message + "</div>"
-                    );
-                    }
-
+                console.log(data);
+                $("form").html(
+                    '<div class="alert alert-danger">Form inputs were wrong, please try again.</div>'
+                );
             } else {
                 $("form").html(
                     '<div class="alert alert-success">' + data.message + "</div>"
@@ -46,6 +33,7 @@ $(document).ready(function () {
             }
         })
         .fail(function (data) {
+            console.log(data);
             $("form").html(
                 '<div class="alert alert-danger">Could not reach server, please try again later.</div>'
             );
